6f680a739c02ea8a8c3bda105c928397ee6ec77b
hilotala0724 <hiiro000724@gmail.com>
2026-05-01 17:39:14 +0900

    Implementing Hilt 2

--- app/src/main/java/com/example/lineapp/LineApplication.kt
+++ app/src/main/java/com/example/lineapp/LineApplication.kt
@@ -1,7 +1,9 @@
 package com.example.lineapp
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class LineApplication : Application() {
 
     override fun onCreate() {
